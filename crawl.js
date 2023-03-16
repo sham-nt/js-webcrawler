59b3406477061872a33a6aa69a67cab8c8bd8a40
sham-nt <90405823+sham-nt@users.noreply.github.com>
2023-03-16 23:37:45 +0530

    Added report.js and completed the main.js

--- crawl.js
+++ crawl.js
@@ -1,5 +1,54 @@
 const { JSDOM } = require('jsdom')
 
+async function crawlPage(baseURL, currentURL, pages){
+    // if this is an offsite URL, bail immediately
+    const currentUrlObj = new URL(currentURL)
+    const baseUrlObj = new URL(baseURL)
+    if (currentUrlObj.hostname !== baseUrlObj.hostname){
+      return pages
+    }
+    
+    const normalizedURL = normalizeURL(currentURL)
+  
+    // if we've already visited this page
+    // just increase the count and don't repeat
+    // the http request
+    if (pages[normalizedURL] > 0){
+      pages[normalizedURL]++
+      return pages
+    }
+  
+    // initialize this page in the map
+    // since it doesn't exist yet
+    pages[normalizedURL] = 1
+  
+    // fetch and parse the html of the currentURL
+    console.log(`crawling ${currentURL}`)
+    let htmlBody = ''
+    try {
+      const resp = await fetch(currentURL)
+      if (resp.status > 399){
+        console.log(`Got HTTP error, status code: ${resp.status}`)
+        return pages
+      }
+      const contentType = resp.headers.get('content-type')
+      if (!contentType.includes('text/html')){
+        console.log(`Got non-html response: ${contentType}`)
+        return pages
+      }
+      htmlBody = await resp.text()
+    } catch (err){
+      console.log(err.message)
+    }
+  
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+    for (const nextURL of nextURLs){
+      pages = await crawlPage(baseURL, nextURL, pages)
+    }
+  
+    return pages
+  }
+
 function normalizeURL (urlString) {
     // normalize url : relative or absolute path, ending with or without '/' , Capitalization of text
     const urlObj = new URL(urlString)
@@ -38,5 +87,6 @@ function getURLsFromHTML(htmlBody,baseURL) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
